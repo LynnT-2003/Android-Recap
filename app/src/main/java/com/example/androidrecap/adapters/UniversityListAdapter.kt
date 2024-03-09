6ab552d1c28cd02ee5b49f0ab33672465a9be8c6
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-03-09 21:31:59 +0700

    Completed Adapter Class with all members implemented.

--- app/src/main/java/com/example/androidrecap/adapters/UniversityListAdapter.kt
+++ app/src/main/java/com/example/androidrecap/adapters/UniversityListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.androidrecap.adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidrecap.databinding.ItemUniversityBinding
@@ -8,19 +9,20 @@ import com.example.androidrecap.models.University
 
 /*
 1. Accept list of University Model Object in Adapter
-2. Create ViewHolder (has only one value which is binding of the item to populate
+2. Create ViewHolder (has only one value which is binding of the item to populate)
 3. Extend appropriately for Adapter and ViewHolder
  */
+
 class UniversityListAdapter(private val universities: List<University>) :
     RecyclerView.Adapter<UniversityListAdapter.UniversityViewHolder>() {
 
     class UniversityViewHolder(val binding: ItemUniversityBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-    }
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UniversityViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemUniversityBinding.inflate(layoutInflater, parent, false)
+        return UniversityViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -28,6 +30,10 @@ class UniversityListAdapter(private val universities: List<University>) :
     }
 
     override fun onBindViewHolder(holder: UniversityViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        // assign data into viewHolder in this function
+        val university = universities[position]
+
+        holder.binding.tvUniversityName.text = university.name
+        holder.binding.tvUniversityProvince.text = university.`state-province`
     }
 }
