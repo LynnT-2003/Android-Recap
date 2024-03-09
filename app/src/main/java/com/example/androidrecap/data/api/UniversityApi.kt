fa4d40a8f5a5dacec77c29dd35b2d28b5a2614fd
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-03-09 18:51:54 +0700

    Update to use Model Object.

--- app/src/main/java/com/example/androidrecap/data/api/UniversityApi.kt
+++ app/src/main/java/com/example/androidrecap/data/api/UniversityApi.kt
@@ -1,12 +1,13 @@
 package com.example.androidrecap.data.api
 
+import com.example.androidrecap.models.University
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 
 class UniversityApi: BaseApi() {
     // need model here (list of <Model> Object)
-    suspend fun getUniversities(): List<String> =
+    suspend fun getUniversities(): List<University> =
         client
             .get("http://universities.hipolabs.com/search?country=Thailand")
-            .body<List<String>>() // explicitly define type of response (List of Objects)
+            .body<List<University>>() // explicitly define type of response (List of Objects)
 }
