66bc6dc0f8d86b2f3bf76c8f57489cd2fbd7397a
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-03-10 19:58:12 +0700

    Successfully got current GPS location on GMaps.

--- app/src/main/java/com/example/androidrecap/GoogleMapActivity.kt
+++ app/src/main/java/com/example/androidrecap/GoogleMapActivity.kt
@@ -3,8 +3,15 @@ package com.example.androidrecap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
+import androidx.activity.viewModels
 import com.example.androidrecap.databinding.ActivityGoogleMapBinding
+import com.example.androidrecap.utils.PermissionHandler
+import com.example.androidrecap.viewModels.GPSViewModel
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
 
 class GoogleMapActivity : AppCompatActivity() {
     private val view: ActivityGoogleMapBinding by lazy {
@@ -12,16 +19,25 @@ class GoogleMapActivity : AppCompatActivity() {
             layoutInflater
         )
     }
-
+    private val viewModel: GPSViewModel by viewModels()
     private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(view.root)
+        PermissionHandler.requestPermissionIfRequired(this, PermissionHandler.GPS)
+        viewModel.getCurrentLocation()
+
         view.mvGoogleMap.onCreate(savedInstanceState)
 
-        view.mvGoogleMap.getMapAsync{
+        view.mvGoogleMap.getMapAsync {
             map = it
+        }
+
+        viewModel.userLocation.observe(this@GoogleMapActivity) {
+            Log.d("DAT", "TRIGGEr")
+            val latLng = LatLng(it.latitude, it.longitude)
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 0f))
         }
     }
 
