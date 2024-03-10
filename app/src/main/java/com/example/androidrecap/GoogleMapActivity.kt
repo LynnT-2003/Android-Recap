88a10db831b7de9d4ed9dd431f2278520f5c80fa
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-03-10 19:03:52 +0700

    Initialized Google Maps setup for Android.

--- app/src/main/java/com/example/androidrecap/GoogleMapActivity.kt
+++ app/src/main/java/com/example/androidrecap/GoogleMapActivity.kt
@@ -2,10 +2,20 @@ package com.example.androidrecap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidrecap.databinding.ActivityGoogleMapBinding
+import com.google.android.gms.maps.GoogleMap
 
 class GoogleMapActivity : AppCompatActivity() {
+    private val view: ActivityGoogleMapBinding by lazy {
+        ActivityGoogleMapBinding.inflate(
+            layoutInflater
+        )
+    }
+    private lateinit var map: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_google_map)
+        setContentView(view.root)
+
     }
 }
