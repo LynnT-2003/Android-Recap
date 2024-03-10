dada93b134241005ca5b579b3b267a8d4c810e70
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-03-10 19:07:44 +0700

    Lifecycle for google maps.

--- app/src/main/java/com/example/androidrecap/GoogleMapActivity.kt
+++ app/src/main/java/com/example/androidrecap/GoogleMapActivity.kt
@@ -2,6 +2,7 @@ package com.example.androidrecap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.example.androidrecap.databinding.ActivityGoogleMapBinding
 import com.google.android.gms.maps.GoogleMap
 
@@ -16,6 +17,41 @@ class GoogleMapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(view.root)
+        view.mvGoogleMap.onCreate(savedInstanceState)
+    }
 
+    override fun onStart() {
+        super.onStart()
+        view.mvGoogleMap.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        view.mvGoogleMap.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        view.mvGoogleMap.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        view.mvGoogleMap.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        view.mvGoogleMap.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        view.mvGoogleMap.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        view.mvGoogleMap.onSaveInstanceState(outState)
     }
 }
