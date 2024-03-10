f926bc39b4d27308d377e3e1492df9986ae3ab44
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-03-11 03:14:32 +0700

    Finished whatever Firebase was taught in class.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,8 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.2.0")
+    implementation("com.google.firebase:firebase-common-ktx:20.4.2")
+    implementation("com.google.firebase:firebase-database-ktx:20.3.1")
     implementation("com.google.firebase:firebase-database:20.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/androidrecap/FirebaseActivity.kt
+++ app/src/main/java/com/example/androidrecap/FirebaseActivity.kt
@@ -2,7 +2,16 @@ package com.example.androidrecap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.androidrecap.databinding.ActivityFirebaseBinding
+import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
+import com.google.gson.Gson
+import java.time.LocalDateTime
 
 class FirebaseActivity : AppCompatActivity() {
     private val view: ActivityFirebaseBinding by lazy {
@@ -11,8 +20,51 @@ class FirebaseActivity : AppCompatActivity() {
         )
     }
 
+    data class FirebaseStudent(
+        val studentId: Int? = null,
+        val name: String? = null,
+        val grade: Int? = null
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(view.root)
+        setContentView(R.layout.activity_firebase)
+
+        val database = Firebase.database(
+            "https://androidrecap-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        )
+
+        val ref = database.getReference("message")
+        val studentRef = database.getReference("students").child("1")
+
+        studentRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val student = snapshot.getValue(FirebaseStudent::class.java)
+                Toast.makeText(
+                    this@FirebaseActivity,
+                    "Student: ${student?.name}, Grade: ${student?.grade}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        ref.setValue("Current time is ${LocalDateTime.now()}")
+
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val message = snapshot.value.toString()
+                Toast.makeText(this@FirebaseActivity, message, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
