17ae86cfabda19dc27040b64ff726ff0a45b7f0b
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-03-11 03:39:01 +0700

    Completed getting current location.

--- app/src/main/java/com/example/androidrecap/GPSActivity.kt
+++ app/src/main/java/com/example/androidrecap/GPSActivity.kt
@@ -2,12 +2,35 @@ package com.example.androidrecap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import com.example.androidrecap.databinding.ActivityGpsactivityBinding
+import com.example.androidrecap.utils.PermissionHandler
+import com.example.androidrecap.viewModels.GPSViewModel
 
 class GPSActivity : AppCompatActivity() {
-    val view: ActivityGpsactivityBinding by lazy { ActivityGpsactivityBinding.inflate(layoutInflater) }
+    private val view: ActivityGpsactivityBinding by lazy { ActivityGpsactivityBinding.inflate(layoutInflater) }
+    private val viewModel: GPSViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(view.root)
+        PermissionHandler.requestPermissionIfRequired(this, PermissionHandler.GPS)
+
+        view.btnGetLocation.setOnClickListener {
+            viewModel.getCurrentLocation()
+        }
+
+        viewModel.userLocation.observe(this) {
+            view.tvUserLocation.text = "User Location: ${it.latitude} ${it.longitude}"
+        }
+
+        viewModel.error.observe(this) {
+            AlertDialog.Builder(this)
+                .setTitle("Something went wrong, are you on Jupiter?")
+                .setMessage(it.message)
+                .setPositiveButton("Ok") {dialog, _ -> dialog.cancel()}
+                .show()
+        }
     }
 }
