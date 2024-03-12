617058bb9ff4cdf9a3a408ddff2d74449b1bde86
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-03-13 05:45:05 +0700

    Completed Bottom Navigation View with replaceFragment.

--- app/src/main/java/com/example/androidrecap/BottomNavigationActivity.kt
+++ app/src/main/java/com/example/androidrecap/BottomNavigationActivity.kt
@@ -2,7 +2,11 @@ package com.example.androidrecap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.androidrecap.databinding.ActivityBottomNavigationBinding
+import com.example.androidrecap.fragments.FragmentA
+import com.example.androidrecap.fragments.FragmentB
+import com.example.androidrecap.fragments.FragmentC
 
 class BottomNavigationActivity : AppCompatActivity() {
     private val view: ActivityBottomNavigationBinding by lazy {
@@ -14,5 +18,23 @@ class BottomNavigationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(view.root)
+
+        view.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.item_home -> changeFragment(FragmentA())
+                R.id.item_profile -> changeFragment(FragmentB())
+                R.id.item_settings -> changeFragment(FragmentC())
+                else -> false
+            }
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment): Boolean {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainerView1, fragment)
+            .addToBackStack(fragment::class.java.name)
+            .commit()
+        return true
     }
 }
